9ff8ec129c4517053a2816dfc95cb7551e0dd0d7
projec1707 <pwroma@gmail.com>
2023-02-16 14:17:15 +0200

    Initial Commit

---
+++ CalculatorLightDarkMode/CalculatorLightDarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorLightDarkModeApp.swift
+//  CalculatorLightDarkMode
+//
+//  Created by Roman Riepa on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorLightDarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
