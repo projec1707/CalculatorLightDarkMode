e3c1ffbc4378ab86de16fab124ceb3717343201c
projec1707 <pwroma@gmail.com>
2023-02-16 15:00:30 +0200

    Create Project and all Colors

---
+++ CalculatorLightDarkMode/image Constants.swift
@@ -0,0 +1,18 @@
+//
+//  image Constants.swift
+//  CalculatorLightDarkMode
+//
+//  Created by Roman Riepa on 16.02.2023.
+//
+
+import Foundation
+import SwiftUI
+
+let buttonBackgroundColor = Color("buttonBackground")
+let foregroundDigitsColor = Color("foregroundDigits")
+let foregroundRightButtonsColor = Color("foregroundRightButtons")
+let foregroundTopButtonsColor = Color("foregroundTopButtons")
+let primaryBackgroundColor = Color("primaryBackground")
+let secondaryBackgroundColor = Color("secondaryBackground")
+let sunOrMoonNotSelectedColor = Color("sunOrMoonNotSelected")
+let sunOrMoonSelectedColor = Color("sunOrMoonSelected")
